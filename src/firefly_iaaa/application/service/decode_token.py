99e4cfea87a24b0c994134dabb0e0fb0fc7c97b8
Jamey Boyett <jamey.boyett@pwrlab.com>
2022-02-22 10:07:59 -0800

    Returns an error message instead of Erroring out for Decoding token

--- src/firefly_iaaa/application/service/decode_token.py
+++ src/firefly_iaaa/application/service/decode_token.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import firefly as ff
+from jwt import ExpiredSignatureError
 
 from firefly_iaaa import domain
 
@@ -10,4 +11,7 @@ class DecodedToken(ff.ApplicationService):
     _decode_token: domain.DecodeToken = None
 
     def __call__(self, token: str, audience: str = None, **kwargs):
-        return self._decode_token(token, audience)
+        try:
+            return self._decode_token(token, audience)
+        except ExpiredSignatureError:
+            return {'message': 'error', 'error': 'Expired Token'}
