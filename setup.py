2af5d58bcd51ef40d4dbf1281ea436c28e83a9cd
jdwil <jdwilliams1982@gmail.com>
2022-03-08 14:05:52 -0600

    New release

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='firefly-iaaa',
-    version='0.1.21',
+    version='0.1.22',
     author="JD Williams",
     author_email="me@jdwilliams.xyz",
     description="Bounded context for users of your application.",
